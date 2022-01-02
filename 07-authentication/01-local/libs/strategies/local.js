8ec1d22b3acc83492b6c7cff5d2c7d53eae83960
Kazachenko Viacheslav <kreeon@ya.ru>
2022-01-02 18:46:28 +0300

    06-mongodb-mongoose: Добавляет локальную стратегию аутентификации

--- 07-authentication/01-local/libs/strategies/local.js
+++ 07-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,19 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+    async function(email, password, done) {
+      const user = await User.findOne({email}).exec();
+      if (user) {
+        const isPasswordCorrect = await user.checkPassword(password);
+        if (isPasswordCorrect) {
+          done(null, user);
+        } else {
+          done(null, false, 'Неверный пароль');
+        }
+      }
+
+      done(null, false, 'Нет такого пользователя');
     },
 );
