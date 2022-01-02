4c385a4818d0fd64524948b345d3438bdfb9f5da
Kazachenko Viacheslav <kreeon@ya.ru>
2022-01-03 00:39:14 +0300

    07-authentication: фиксит многократный вызов done

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,7 +1,7 @@
 const User = require('../../models/User');
 module.exports = async function authenticate(strategy, email, displayName, done) {
   if (!email) {
-    done(null, null, 'Не указан email');
+    return done(null, null, 'Не указан email');
   }
   const user = await User.findOne({email}).exec();
   if (user) {
